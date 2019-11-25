aaf269bbdbb59ab25e7358cd0a7dfd7a444257f1
Wweakness <770199484@qq.com>
2019-11-25 22:55:56 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//镜像二叉树
+ class TreeNode {
+ int val = 0;
+ TreeNode left = null;
+ TreeNode right = null;
+
+ public TreeNode(int val) {
+ this.val = val;
+
+ }
+
+ }
+
+public class Main {
+    public void Mirror(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        if(root.left==null&&root.right==null){
+            return;
+        }
+        TreeNode temp;
+        temp= root.left;
+        root.left=root.right;
+        root.right=temp;
+        if(root.left!=null){
+            Mirror(root.left);
+        }
+        if(root.right!=null){
+            Mirror(root.right);
+        }
+
+
+    }
+}
